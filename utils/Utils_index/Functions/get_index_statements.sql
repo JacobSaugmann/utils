2464e6722f20cfaa043e6c3441cb244b73981359
Jacob Saugmann <jsa@Unit-it.dk>
2020-12-22 08:18:04 +0100

    Added trigger for disable/enable indexes

--- utils/Utils_index/Functions/get_index_statements.sql
+++ utils/Utils_index/Functions/get_index_statements.sql
@@ -8,7 +8,7 @@ RETURN(
 SELECT 'USE '+ QUOTENAME(database_name) + '; ' + create_stmt AS create_stmt,
 	   'USE '+ QUOTENAME(database_name) + '; ' + drop_statement AS drop_statement,
 	   'USE '+ QUOTENAME(database_name) + '; ' + ' ALTER INDEX '+QUOTENAME(index_name) +' ON '+QUOTENAME(schema_name)+'.' +QUOTENAME(table_name) + ' DISABLE;' AS disblae_stmt,
-	   'USE '+ QUOTENAME(database_name) + '; ' + ' ALTER INDEX '+QUOTENAME(index_name) +' ON '+QUOTENAME(schema_name)+'.' +QUOTENAME(table_name) AS enable_stmt
+	   'USE '+ QUOTENAME(database_name) + '; ' + ' ALTER INDEX '+QUOTENAME(index_name) +' ON '+QUOTENAME(schema_name)+'.' +QUOTENAME(table_name) + ' REBUILD;' AS enable_stmt
 FROM util_index.indexes i
 WHERE i.id = @id
 )
---
+++ utils/Utils_index/Triggers/tgr_enableindex.sql
@@ -0,0 +1,36 @@
+﻿CREATE TRIGGER [util_index].[tgr_enableindex] 
+   ON  [util_index].[indexes]
+   AFTER UPDATE
+AS 
+BEGIN
+
+	SET NOCOUNT ON;
+	   
+	DECLARE @disable_before BIT, @disable_updated BIT
+	SELECT @disable_before = del.[disable]
+	FROM  deleted del
+           
+
+
+	SELECT @disable_updated = [disable]
+	FROM inserted
+	 
+	--DECLARE @msg NVARCHAR(200)
+	--SET @msg = CONCAT('value before ', @disable_before,' value after ', @disable_updated)
+	--RAISERROR(@msg, 0,1) WITH LOG
+
+
+	IF(@disable_before = 1 AND @disable_updated = 0)
+	BEGIN
+		
+		DECLARE @sql_query NVARCHAR(4000)
+		SELECT @sql_query = ix.enable_stmt
+		FROM inserted i 
+		CROSS APPLY util_index.get_index_statements(i.id) AS ix
+
+		EXEC sp_executesql @sql_query
+
+	END
+
+
+END
